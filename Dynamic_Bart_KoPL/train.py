019525cff3dbc49f529c1256f151cb3e00c9bae3
zjtfo <48672420+zjtfo@users.noreply.github.com>
2022-09-30 16:51:23 +0800

    Update train.py

--- Dynamic_Bart_KoPL/train.py
+++ Dynamic_Bart_KoPL/train.py
@@ -430,7 +430,7 @@ def train(args):
 def main():
     parser = argparse.ArgumentParser()
     # input and output
-    parser.add_argument('--input_dir', default='./Bart_Program/preprocessed_data')
+    parser.add_argument('--input_dir', default='./dynamic_bart_kopl/preprocessed_data')
     parser.add_argument('--output_dir', default='./dynamic_bart_kopl/output/checkpoint')
 
     parser.add_argument('--save_dir', default='./dynamic_bart_kopl/log', help='path to save checkpoints and logs')
